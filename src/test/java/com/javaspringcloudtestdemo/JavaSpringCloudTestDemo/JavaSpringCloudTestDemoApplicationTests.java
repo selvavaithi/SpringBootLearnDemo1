946ffe8ea0d65173f40b4963b2cd7aa48b096d67
adeunqa_selva <selva.kumar@adeunqa.com>
2024-11-20 14:52:41 +0530

    first commit

---
+++ src/test/java/com/javaspringcloudtestdemo/JavaSpringCloudTestDemo/JavaSpringCloudTestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaspringcloudtestdemo.JavaSpringCloudTestDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringCloudTestDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
